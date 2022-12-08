4ab58996e07498a47734c8ed58204c51b45e0011
kachilenga <kachilengabanda@gmail.com>
2022-12-08 21:47:08 +0000

    update puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,11 @@
  *
  * Return 0
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
 
