060764658ec50f788f932828ae68ad340bf9791b
kachilenga <kachilengabanda@gmail.com>
2022-12-08 21:39:29 +0000

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main function -Starting point
+ *
+ * Return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
