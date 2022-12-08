b338de05cf0b8fecf4dc9fc9eaf0c07cf7ec0faf
kachilenga <kachilengabanda@gmail.com>
2022-12-08 22:06:35 +0000

    Compiler

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main function
+ * description : starting point
+ *
+ * Return 0 -success*
+ * use to return int
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
