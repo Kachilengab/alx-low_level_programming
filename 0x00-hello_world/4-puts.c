ed53a92171ed067237f732f412353969e4baa42b
kachilenga <kachilengabanda@gmail.com>
2022-12-08 23:07:46 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * int main -Entry point of the programme
  *
+ * Description : This is the main programme where
+ * everything starts from
  * Return :Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
+/**
+ * Return : Returns value of 0 to the user
+ *
+ * Description: this is what returns and int value
+ */
 	return (0);
 }
