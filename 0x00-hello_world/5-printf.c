deb555dd777522bd66825573967b01ca2f2dcd31
kachilenga <kachilengabanda@gmail.com>
2022-12-10 08:30:43 +0000

    5-print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,9 +8,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-/**
- * Return : For returining
- * Description: this does a whole lot of it
- */
 	return (0);
 }
