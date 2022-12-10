8f078ed9f45dcb81424c80f561cc8383d9e4ff01
kachilenga <kachilengabanda@gmail.com>
2022-12-10 05:49:38 +0000

    2

--- 0x00-hello_world/2-main.c
+++ 0x00-hello_world/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main -Entry point
  *
- * Return: Always 0 (Success)
+ * Return :Always 0 (Success)
  */
 int main(void)
 {
-	printf("holberton School");
+	printf("Holberton School\n");
 	return (0);
 }
