b1280b28cd9ec8c6d34d744d8af08b223e35acd1
kachilenga <kachilengabanda@gmail.com>
2022-12-11 08:08:39 +0000

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
- * 
- * Return :Always 0 (Success)
+ * main -Entry point of the programme.
+ * @void: returns void
+ *
+ * Description: The main functions is where
+ * the entry of the programme lies
+ *
+ * Return: Always 0 (Success) to show that the
+ * programe executed successfully
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr ,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
