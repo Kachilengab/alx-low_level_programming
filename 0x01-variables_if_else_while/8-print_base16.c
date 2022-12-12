a6ba273fe4ea4219ba7dde1dca49c7cfadf11aae
kachilenga <kachilengabanda@gmail.com>
2022-12-12 14:24:00 +0000

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/** more headers goes there*/
+/**
+ * main -Entry point
+ * Description: this is where the programme
+ * first starts executing
+ *
+ * Return: Return (0) Always (Success)
+ */
+int main(void)
+{
+	char i;
+
+	int j;
+
+	i = 'a';
+	j = 0;
+
+	while 
+		(j < 10) {
+		putchar(j + '0');
+		j++;
+		}
+	while
+		(i <= 'f') {
+			putchar(i);
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
