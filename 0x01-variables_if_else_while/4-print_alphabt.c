94c0dc7d8c0f6a7cfd5847e21349b3a0146529af
kachilenga <kachilengabanda@gmail.com>
2022-12-12 14:48:10 +0000

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/** more headers goes there*/
+/**
+ * main -Entry point
+ * Description: this is where the programme
+ * first starts executing
+ *
+ * Return: Return (0) Always (Success)
+ */
+int main(void)
+{
+	char i;
+
+	i = 'a';
+	while
+		(i <= 'z') {
+			if ((i != 'q' && i != 'e') && i < 'z')
+				putchar(i);
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
