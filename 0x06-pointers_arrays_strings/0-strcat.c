03d2838a933fd8b60095f9a2fc3d0192fc9955e3
kachilenga <kachilengabanda@gmail.com>
2023-01-19 11:29:21 +0000

    1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _strcat - prints a strings to stdout
  * @dest: what to print
@@ -23,7 +22,7 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
+	}
 
 	dest[i] = '\0';
 	return (dest);
