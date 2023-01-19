7ea47e2212e69167fd0f9bf38bce3f7076f34476
kachilenga <kachilengabanda@gmail.com>
2023-01-19 10:18:26 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints a strings to stdout
+ * @n: what to print
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
